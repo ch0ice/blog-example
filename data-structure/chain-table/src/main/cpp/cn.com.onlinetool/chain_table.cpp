f4b7ecb2984c6182611c65f36356259fe020713e
liuzy <liuzy@mapbar.com>
2018-07-27 19:07:24 +0800

    update linked_list implement

--- data-structure/chain-table/src/main/cpp/cn.com.onlinetool/chain_table.cpp
+++ data-structure/chain-table/src/main/cpp/cn.com.onlinetool/chain_table.cpp
@@ -2,30 +2,35 @@
 using namespace std;
 
 //单向链表
-struct ss
+struct node
 {
     //数据域
     int data;
     //指针域
-    ss* next;
+    node* next;
 };
 
 int main()
 {
 
-    ss* s1 = new ss();
+    node* s1 = new node();
     s1->data = 1;
-    ss* s2 = new ss();
+    node* s2 = new node();
     s2->data = 2;
-    ss* s3 = new ss();
+    node* s3 = new node();
     s3->data = 3;
 
 //  建立链接
     s1->next = s2;
     s2->next = s3;
 
+//  打印数据
+    cout << s1->data << endl;
+    cout << s1->next->data << endl;
+    cout << s1->next->next->data << endl;
+
 //  插入节点
-    ss* s4 = new ss();
+    node* s4 = new node();
     s4->data = 40;
     s1->next = s4;
     s4->next = s2;
@@ -40,6 +45,7 @@ int main()
     cout << s1->data << endl;
     cout << s1->next->data << endl;
     cout << s1->next->next->data << endl;
+    cout << s1->next->next->next->data << endl;
 
     delete s1;
     delete s2;
