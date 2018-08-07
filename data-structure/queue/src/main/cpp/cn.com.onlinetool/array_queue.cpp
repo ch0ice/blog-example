47344f405479cfe2a8f387843e3bd9b74843cfdc
Choice <mr_liuzeyuan@163.com>
2018-08-07 18:14:37 +0800

    modify by choice reason:update queue model

--- data-structure/queue/src/main/cpp/cn.com.onlinetool/array_queue.cpp
+++ data-structure/queue/src/main/cpp/cn.com.onlinetool/array_queue.cpp
@@ -5,6 +5,7 @@
 #include <cstdlib>
 using namespace std;
 
+#define MAX_QSIZE 10 // 最大队列长度+1
 // 阵列队列的存储结构
 struct Queue {
     int Array[10]; // 阵列空间大小
@@ -20,18 +21,21 @@ Queue* Q_Init() {
         // 存储分配失败
         exit(OVERFLOW);
     }
+    //初始化
     Q->head = Q->tail = Q->length = 0;
     return Q;
 }
 
 // 将Q清为空队列
 void Q_Clear(Queue *Q) {
+    //清除头尾下标和长度
     Q->head = Q->tail = Q->length = 0;
 }
 
 // 入列
 int Q_Put(Queue *Q, int x) {
-    if (Q->tail + 1 == 10) {
+    //如果当前元素数量等于最大数量 返回 -1
+    if (Q->tail + 1 == MAX_QSIZE) {
         return -1;
     }
     Q->Array[Q->tail] = x;
@@ -43,12 +47,11 @@ int Q_Put(Queue *Q, int x) {
 
 // 出列
 int Q_Poll(Queue *Q) {
+    //如果当前元素数量等于最大数量 返回 -1
+    if (Q->head + 1 == MAX_QSIZE)
+        return -1;
     int x = Q->Array[Q->head];
-    // 空间大小10
-    if (Q->head + 1 == 10)
-        Q->head = 0;
-    else
-        Q->head = Q->head + 1;
+    Q->head = Q->head + 1;
     // 移出后減少1
     Q->length = Q->length - 1;
     return x;
