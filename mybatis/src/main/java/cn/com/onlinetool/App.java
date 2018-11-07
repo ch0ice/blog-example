fe18eb26feea388d90277097c963b1c780db539e
Choice <mr_liuzeyuan@163.com>
2018-11-07 20:15:48 +0800

    modify by choice reason:add by cat and mybatis model

--- mybatis/src/main/java/cn/com/onlinetool/App.java
+++ mybatis/src/main/java/cn/com/onlinetool/App.java
@@ -1,13 +1,21 @@
 package cn.com.onlinetool;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+@MapperScan("cn.com.onlinetool.asd")
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
+        SpringApplication app = new SpringApplication(App.class);
+        app.run();
         System.out.println( "Hello World!" );
     }
 }
