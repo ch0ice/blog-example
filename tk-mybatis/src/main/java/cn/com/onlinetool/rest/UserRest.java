7f318aae85780583fc9e10428266ece57d54a6a7
Choice <mr_liuzeyuan@163.com>
2018-11-09 15:33:23 +0800

    modify by choice reason: add test code

--- tk-mybatis/src/main/java/cn/com/onlinetool/rest/UserRest.java
+++ tk-mybatis/src/main/java/cn/com/onlinetool/rest/UserRest.java
@@ -3,6 +3,12 @@ package cn.com.onlinetool.rest;
 import cn.com.onlinetool.common.base.BaseRest;
 import cn.com.onlinetool.dto.UserDTO;
 import cn.com.onlinetool.service.UserService;
+import com.alibaba.fastjson.JSON;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author choice
@@ -10,5 +16,20 @@ import cn.com.onlinetool.service.UserService;
  * @date 2018/11/9 下午2:45
  *
  */
+@RestController
+@RequestMapping("/api")
 public class UserRest extends BaseRest<UserService, UserDTO> {
+
+
+    @GetMapping("/test")
+    public String test(){
+        List<UserDTO> userList = myService.find();
+        return JSON.toJSONString(userList);
+    }
+
+    @GetMapping("/example")
+    public String testExample(){
+
+        return myService.test();
+    }
 }
--- tk-mybatis/src/main/java/cn/com/onlinetool/service/UserService.java
+++ tk-mybatis/src/main/java/cn/com/onlinetool/service/UserService.java
@@ -4,8 +4,12 @@ import cn.com.onlinetool.common.base.BaseService;
 import cn.com.onlinetool.dto.UserDTO;
 import cn.com.onlinetool.entity.User;
 import cn.com.onlinetool.mapper.UserMapper;
+import com.alibaba.fastjson.JSON;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
 
 /**
  * @author choice
@@ -16,4 +20,10 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(readOnly = true,rollbackFor = Exception.class)
 public class UserService extends BaseService<UserMapper, User, UserDTO> {
+    public String test(){
+        Example example = new Example(User.class);
+        example.createCriteria().andEqualTo("password","123");
+        List<User> users = mapper.selectByExample(example);
+        return JSON.toJSONString(users);
+    }
 }
