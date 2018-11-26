6f173ee25e2af13290f74649febdde380e51200e
Choice <mr_liuzeyuan@163.com>
2018-11-26 16:27:24 +0800

    modify by choice reason: add cat model

--- cat/src/main/java/cn/com/onlinetool/App.java
+++ cat/src/main/java/cn/com/onlinetool/App.java
@@ -1,18 +1,16 @@
 package cn.com.onlinetool;
 
-import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  *
  */
-@MapperScan("cn.com.onlinetool.sd")
 @SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
